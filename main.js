37018bd4e1f63bc353cbd505e7edcce9c6521c2a
unknown <diana.chorna@gmail.com>
2021-06-01 19:30:12 +0300

    a

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+      $(".example").circlos();
+});
+
+$(document).ready(function(){
+      $(".example").circlos({
+        backgroundColor: '#b3cef6',
+        progressColor: '#4b86db',
+        percent: 75, 
+        duration: 2000 
+      });
+    });
